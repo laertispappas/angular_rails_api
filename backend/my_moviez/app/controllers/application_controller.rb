fcc8bc7283f609c1f238ebe86a9696c1cc868f78
Laertis Pappas <laertis.pappas@gmail.com>
2016-09-06 00:26:42 +0300

    Adds authentication endpoints

--- backend/my_moviez/app/controllers/application_controller.rb
+++ backend/my_moviez/app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_request
+
+  def current_user
+    @current_user ||= unauthorized!
+  end
+  helper_method :current_user
+
+  private
+  def authenticate_request
+    @current_user = AuthorizeApiRequest.call(request.headers).result
+    unauthorized! unless @current_user
+  end
+
+  def unauthorized!
+    render json: { error: 'Not Authorized' }, status: 401
+  end
 end
