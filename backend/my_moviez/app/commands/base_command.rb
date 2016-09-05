ef1c7069ed85c9d52f1d6a68a222481f297a7a11
Laertis Pappas <laertis.pappas@gmail.com>
2016-09-06 00:04:21 +0300

    Adds command classes for authentication and authorization

---
+++ backend/my_moviez/app/commands/base_command.rb
@@ -0,0 +1,50 @@
+class BaseCommand
+  attr_reader :result
+
+  class << self
+    # DoAuthentication.call(email, passwd)
+    def call(*args)
+      new(*args).call
+    end
+  end
+
+  def call
+    raise NotImplementedError unless defined?(super)
+    @called = true
+    @result = super
+
+    self
+  end
+
+  def success?
+    called? && !failure?
+  end
+
+  def failure?
+    called? && errors.any?
+  end
+
+  def errors
+    @errors ||= Error.new
+  end
+
+  private
+
+  def called?
+    @called ||= false
+  end
+
+  class Error < Hash
+    def add(key, value, opts = {})
+      self[key] ||= []
+      self[key] << value
+      self[key].uniq!
+    end
+
+    def each
+      each_key do |field|
+        self[field].each { |msg| yield field, msg }
+      end
+    end
+  end
+end
