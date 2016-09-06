56e544972ecaebdea19d9c371149501545e405b4
Laertis Pappas <laertis.pappas@gmail.com>
2016-09-07 00:02:36 +0300

    Updates signup controller adds bootstrap directive

--- frontend/app/scripts/controllers/signup.js
+++ frontend/app/scripts/controllers/signup.js
@@ -8,7 +8,7 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('SignupCtrl', function ($scope, $http, $log) {
+  .controller('SignupCtrl', function ($scope, $http, $log, alertService, $location, userService) {
     $scope.signup = function() {
       var payload = {
         email: $scope.email,
@@ -18,8 +18,20 @@ angular.module('frontendApp')
 
       $http.post('/api/registrations', payload).success(function(data) {
         $log.debug(data);
-      }).error(function(data) {
+      }).error(function(data, status) {
         $log.debug(data);
+        if(status === 422) {
+          angular.forEach(data, function(value, key) {
+            if(key === 'email' || key === 'password' || key === 'password_confirmation') {
+              alertService.add('danger', key + ' : ' + value);
+            } else {
+              alertService.add('danger', value.message);
+            }
+          });
+        }
+        if(status == 500) {
+          alertService.add('danger', 'Internal server error!');
+        }
       });
     };
   });
