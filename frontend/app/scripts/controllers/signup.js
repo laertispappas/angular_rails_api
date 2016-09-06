4ba7ac0b3cdacaa8e7682744e7dd613d2b9578ce
Laertis Pappas <laertis.pappas@gmail.com>
2016-09-07 00:16:03 +0300

    Redirect on success do not check property ettr

--- frontend/app/scripts/controllers/signup.js
+++ frontend/app/scripts/controllers/signup.js
@@ -18,11 +18,9 @@ angular.module('frontendApp')
 
       $http.post('/api/registrations', payload).success(function(data) {
         $log.debug(data);
-        if(data.hasOwnProperty('success')) {
-
+          //if(data.hasOwnProperty('success')) {
           userService.username = $scope.email;
           $location.path('/dashboard');
-        }
       }).error(function(data, status) {
         $log.debug(data);
         if(status === 422) {
