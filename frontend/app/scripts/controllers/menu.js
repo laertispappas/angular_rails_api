10b675e2bc4375f94ebeb585f794c7935b6c0223
Laertis Pappas <laertis.pappas@gmail.com>
2016-09-07 00:20:05 +0300

    Adds menu ctrl

---
+++ frontend/app/scripts/controllers/menu.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontendApp.controller:MenuCtrl
+ * @description
+ * # MenuCtrl
+ * Controller of the frontendApp
+ */
+angular.module('frontendApp')
+  .controller('MenuCtrl', function ($scope, $http, userService, $location) {
+    $scope.user = UserService;
+
+    $scope.logout = function() {
+      $http.get('/app/logout').success(function(data){
+        userService.username = '';
+        $location.path('/login')
+      });
+
+      // TODO Remove this add Authentication
+      $scope.$watch('user.username', function(newVal) {
+        if (newVal === '') {
+          $scope.isLoggedIn = false;
+        } else {
+          $scope.userName = newVal;
+          $scope.isLoggedIn = true;
+        }
+      });
+    }
+  });
