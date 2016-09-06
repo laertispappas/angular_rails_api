9ebaa8a8bf5147fda3de3afc4b2e54d01c842372
Laertis Pappas <laertis.pappas@gmail.com>
2016-09-06 23:52:49 +0300

    Adds alert service

---
+++ frontend/app/scripts/services/alerts.js
@@ -0,0 +1,65 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name frontendApp.alerts
+ * @description
+ * # alerts
+ * Service in the frontendApp.
+ */
+// AngularJS will instantiate a singleton by calling "new" on this function
+angular.module('frontendApp')
+//  .service('alerts', function () {
+   .factory('alertService', function($timeout) {
+
+     var ALERT_TIMEOUT = 5000;
+
+     function add(type, msg, timeout) {
+
+       if (timeout) {
+         $timeout(function(){
+           closeAlert(this);
+         }, timeout);
+       } else {
+         $timeout(function(){
+           closeAlert(this);
+         }, ALERT_TIMEOUT);
+       }
+
+       return alerts.push({
+         type: type,
+         msg: msg,
+         close: function() {
+           return closeAlert(this);
+         }
+       });
+     }
+
+     function closeAlert(alert) {
+       return closeAlertIdx(alerts.indexOf(alert));
+     }
+
+     function closeAlertIdx(index) {
+       return alerts.splice(index, 1);
+     }
+
+     function clear(){
+       alerts = [];
+     }
+
+     function get() {
+       return alerts;
+     }
+
+     var service = {
+           add: add,
+           closeAlert: closeAlert,
+           closeAlertIdx: closeAlertIdx,
+           clear: clear,
+           get: get
+         },
+         alerts = [];
+
+     return service;
+   }
+);
