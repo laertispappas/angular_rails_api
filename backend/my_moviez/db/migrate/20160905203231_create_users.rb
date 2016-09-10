bf1ed2cb5b2955f339e5abf2e70f8ac16ee91e35
Laertis Pappas <laertis.pappas@gmail.com>
2016-09-11 00:10:35 +0300

    Adds more user info
    Index and uniq constraint on email
    User demographics

--- backend/my_moviez/db/migrate/20160905203231_create_users.rb
+++ backend/my_moviez/db/migrate/20160905203231_create_users.rb
@@ -1,9 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
+      t.string :email, uniq: true, index: true
       t.string :password_digest
+
+      # Profile data. This could belong to a profile table instead
+      t.string :first_name
+      t.string :last_name
+      t.string :gender
+      t.date :birthday
 
       t.timestamps
     end
--- backend/my_moviez/db/schema.rb
+++ backend/my_moviez/db/schema.rb
@@ -13,11 +13,15 @@
 ActiveRecord::Schema.define(version: 20160905203231) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
     t.string   "email"
     t.string   "password_digest"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "gender"
+    t.date     "birthday"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.index ["email"], name: "index_users_on_email"
   end
 
 end
