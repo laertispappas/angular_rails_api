572f75f4c0a068e3e9f0ed10c0b290b55882dfd7
Laertis Pappas <laertis.pappas@gmail.com>
2016-09-11 01:41:23 +0300

    Adds actors

--- backend/my_moviez/db/schema.rb
+++ backend/my_moviez/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160910223515) do
+ActiveRecord::Schema.define(version: 20160910224050) do
+
+  create_table "actors", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "cities", force: :cascade do |t|
     t.integer "state_id", null: false
