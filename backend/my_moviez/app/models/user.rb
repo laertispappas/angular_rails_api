ae1ac6a49c545f2e23d2de3e8b9862c907d45b03
Laertis Pappas <laertis.pappas@gmail.com>
2016-09-11 02:37:57 +0300

    Adds user / movie relation to ratings

--- backend/my_moviez/app/models/user.rb
+++ backend/my_moviez/app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :ratings
+  belongs_to :city
+
   validates :email, uniqueness: { case_sensitive: false }
-  #validates :username, uniqueness: { case_sensitive: false }
-  #validates :password, length: { in: 6..10 }
   validates :password, presence: true
+  
 end
