f70c0c0010646f755d5c3dca9d07caf352e125d1
Laertis Pappas <laertis.pappas@gmail.com>
2016-09-11 01:40:31 +0300

    Adds null false constrain to movie title

--- backend/my_moviez/db/schema.rb
+++ backend/my_moviez/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160910223313) do
+ActiveRecord::Schema.define(version: 20160910223515) do
 
   create_table "cities", force: :cascade do |t|
     t.integer "state_id", null: false
@@ -25,10 +25,20 @@ ActiveRecord::Schema.define(version: 20160910223313) do
     t.index ["name"], name: "index_countries_on_name"
   end
 
-  create_table "movies", force: :cascade do |t|
-    t.string   "title"
+  create_table "genres", force: :cascade do |t|
+    t.integer  "movie_id",   null: false
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_genres_on_movie_id"
+    t.index ["name"], name: "index_genres_on_name"
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_movies_on_title"
   end
 
   create_table "states", force: :cascade do |t|
