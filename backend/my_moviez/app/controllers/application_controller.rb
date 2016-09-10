21a8c74398ba92d6d48737b0462c49b445c51305
Laertis Pappas <laertis.pappas@gmail.com>
2016-09-10 22:28:24 +0300

    Don't print request headers on API

--- backend/my_moviez/app/controllers/application_controller.rb
+++ backend/my_moviez/app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::API
-  before_action :print_headers
-
-  def print_headers
-    puts "***" * 100
-    p request.authorization
-    puts "***" * 100
-  end
 end
